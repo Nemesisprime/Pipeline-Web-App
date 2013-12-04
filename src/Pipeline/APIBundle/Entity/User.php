c8aa7bf6d0788d662dcfb79fc0085a7cdd0887fb
Dan <dan@contagious.nu>
2013-12-03 20:43:46 -0600

    Addition of FOSUserBundle and FrontendBundle

---
+++ src/Pipeline/APIBundle/Entity/User.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Pipeline\APIBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Pipeline\APIBundle\Entity\UserRepository")
+ * @ORM\Table(name="pl_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="joined_at", type="datetime")
+     */
+    private $joinedAt;
+
+	/**
+	 * Construct (be sure to call parent construct).
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set joinedAt
+     *
+     * @param \DateTime $joinedAt
+     * @return User
+     */
+    public function setJoinedAt($joinedAt)
+    {
+        $this->joinedAt = $joinedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get joinedAt
+     *
+     * @return \DateTime 
+     */
+    public function getJoinedAt()
+    {
+        return $this->joinedAt;
+    }
+}
