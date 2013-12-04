27365a8ddb2aefc61eb65a314c2eb57abdcd2034
Dan <dan@contagious.nu>
2013-12-03 21:48:52 -0600

    Added FOSUserBundle, Skeleton CSS, PHPLess, and the basic front end bundle controller.

--- src/Pipeline/FrontendBundle/Controller/HomeController.php
+++ src/Pipeline/FrontendBundle/Controller/HomeController.php
@@ -5,11 +5,13 @@ namespace Pipeline\FrontendBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class HomeController extends Controller
 {
     /**
      * @Route("")
+     * @Security("has_role('ROLE_USER')")
      * @Template()
      */
     public function indexAction()
