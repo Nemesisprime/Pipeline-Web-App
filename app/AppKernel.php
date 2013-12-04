218e93d59a371f95c14d14f90335155972fc3e9a
Dan <dan@contagious.nu>
2013-12-04 11:46:49 -0600

    Added Guzzle and GuzzleBundle and an API endpoint to FrontendBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,10 +16,12 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Playbloom\Bundle\GuzzleBundle\PlaybloomGuzzleBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             
             new Pipeline\APIBundle\APIBundle(),
             new Pipeline\FrontendBundle\FrontendBundle(),
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Pipeline/FrontendBundle/Gateway/API.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Pipeline/FrontendBundle/Gateway;
+
+use Guzzle\Http\Client;
+
+/**
+ * API Gateway class.
+ * 
+ * @extends Client
+ */
+class API extends Client { 
+    
+    
+    
+}
